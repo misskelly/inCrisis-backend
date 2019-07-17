78d4625fe3009360562bfb976ab35cf07c6d22b2
Sally Haefling <sallyhaefling@gmail.com>
2019-07-16 18:28:57 -0600

    Add location validations and spec
    Co-Authored-By: Corey Sheesley<Corey.Sheesley@gmail.com>

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Location, type: :model do
+  context 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :phone }
+    it { should validate_presence_of :street }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :zip }
+    it { should validate_presence_of :office_hours }
+    it { should validate_presence_of :lat }
+    it { should validate_presence_of :lng }
+  end
 end
