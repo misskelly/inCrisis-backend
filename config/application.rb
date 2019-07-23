cc8620017087bfb4a68f8b26fc6d901a935171e2
Corey Sheesley <corey.sheesley@gmail.com>
2019-07-23 13:26:43 -0600

    Removes '/' at end of incrisis domain, in application.rb CORS
    Co-authored-by: Brennan Duffey <brennanduffey@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module CcsCrossPollBackend
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000', 'https://incrisis.herokuapp.com/'
+        origins 'localhost:3000', 'https://incrisis.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
