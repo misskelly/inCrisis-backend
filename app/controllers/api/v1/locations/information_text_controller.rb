214375560634ceffe96a0e9fd19b42449d364523
Corey Sheesley <43529041+CSheesley@users.noreply.github.com>
2019-07-24 09:24:06 -0600

    Update information_text_controller.rb

--- app/controllers/api/v1/locations/information_text_controller.rb
+++ app/controllers/api/v1/locations/information_text_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Locations::InformationTextController < ApplicationController
 
   def create
     SendTextService.new(strong_params).send_sms
-    render json: {response: 'Text Message Sent' }, :status => 201
+    render json: {response: 'Text Message Sent' }, status: 201
   end
 
   private
