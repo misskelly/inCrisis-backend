e59989c7d0daecf213801b4060c66478f6e1d3c0
Corey Sheesley <corey.sheesley@gmail.com>
2019-07-24 11:10:25 -0600

    Creates Request Spec for GET api/v1/locations

--- spec/requests/api/v1/locations_request_spec.rb
+++ spec/requests/api/v1/locations_request_spec.rb
@@ -1,11 +1,33 @@
-require 'rails_helper'  
+require 'rails_helper'
 
-describe 'Locations API' do 
-  context 'sort' do
-    it 'returns 200 status code' do
-      get '/api/v1/locations/sort' 
+describe 'Locations API' do
+  context 'GET api/v1/locations' do
+    it 'returns information on all Location objects, and a 200 status code' do
+      create_list(:location, 3)
+
+      get '/api/v1/locations'
+
+      locations = JSON.parse(response.body)["locations"]
+
+      first_location = locations.first
+      expected_keys = [
+                        "id",
+                        "name",
+                        "phone",
+                        "street",
+                        "city",
+                        "state",
+                        "zip",
+                        "office_hours",
+                        "lat",
+                        "lng",
+                        "created_at",
+                        "updated_at"
+                      ]
 
       expect(response.status).to eq(200)
+      expect(locations.count).to eq(3)
+      expect(first_location.keys).to eq(expected_keys)
     end
   end
 end
